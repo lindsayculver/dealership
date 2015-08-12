41e3c6fad3fcc7f0079c0df38f5cede4895d278e
Julianna Green & Lindsay Culver <student@epicodus.com>
2015-08-12 09:40:08 -0700

    add method to make cars array psec pass

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -5,6 +5,7 @@ class Dealership
   define_method(:initialize) do |name|
     @name = name
     @id = @@dealerships.length().+(1)
+    @cars = []
 
   end
 
@@ -15,5 +16,8 @@ class Dealership
   define_method(:id) do
     @id
   end
-  
+
+  define_method(:cars) do
+    @cars
+  end
 end
