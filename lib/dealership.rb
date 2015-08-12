86465aee4e693c86bbac4b69a3f57b894edaeb87
Julianna Green & Lindsay Culver <student@epicodus.com>
2015-08-12 09:34:17 -0700

    Add a spec that assigns a unique id number to each dealership

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,11 +1,19 @@
 class Dealership
-  
+
+  @@dealerships = []
+
   define_method(:initialize) do |name|
     @name = name
+    @id = @@dealerships.length().+(1)
 
   end
 
   define_method(:name) do
     @name
   end
+
+  define_method(:id) do
+    @id
+  end
+  
 end
