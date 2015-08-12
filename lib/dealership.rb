676bf51a33cfe0ef290733a5130321291e90ac1a
Julianna Green & Lindsay Culver <student@epicodus.com>
2015-08-12 09:32:27 -0700

    Add a spec that assigns a unique id number to each dealership

---
+++ lib/dealership.rb
@@ -0,0 +1,11 @@
+class Dealership
+  
+  define_method(:initialize) do |name|
+    @name = name
+
+  end
+
+  define_method(:name) do
+    @name
+  end
+end
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -2,12 +2,19 @@ require('rspec')
 require('dealership')
 
 describe(Dealership) do
+
   describe('#name') do
-    it('returns the name of the dealership')
-    test_dealership.Dealership.new(LandJs) do
-      expect(test_dealership.name()).to(eq("LandJs"))
+    it('returns the name of the dealership') do
+      test_dealership = Dealership.new("Flying Dragon Used Cars")
+      expect(test_dealership.name()).to(eq("Flying Dragon Used Cars"))
     end
   end
 
-  
+  describe('#id') do
+    it('returns the id of the dealership') do
+      test_dealership = Dealership.new("Flying Dragon Used Cars")
+      expect(test_dealership.id()).to(eq(1))
+    end
+  end
+
 end
