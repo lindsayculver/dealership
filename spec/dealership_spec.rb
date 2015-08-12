21c2edaedf66084986c30daab8b99f1cbd0f58ba
Julianna Green & Lindsay Culver <student@epicodus.com>
2015-08-12 09:22:36 -0700

    Initial commit, adds gems, and first spec for dealership name.

---
+++ spec/dealership_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('dealership')
+
+describe(Dealership) do
+  describe('#name') do
+    it('returns the name of the dealership')
+    test_dealership.Dealership.new(LandJs) do
+      expect(test_dealership.name()).to(eq("LandJs"))
+    end
+  end
+
+  
+end
