65e0057fc039e1c3cdb9d12564c9fbf4ccb82bb1
lindsay <user@users-MacBook-Pro.local>
2015-08-12 20:59:58 -0700

    add required sinatr files but wont load on localhost

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,17 +1,15 @@
 class Dealership
-
   @@dealerships = []
 
   define_method(:initialize) do |name|
     @name = name
     @id = @@dealerships.length().+(1)
     @cars = []
-
   end
 
-  define_singleton_method(:all) do
-    @@dealerships
-  end
+  define_method(:add_vehicle) do |vehicle|
+  @cars.push(vehicle)
+end
 
   define_method(:name) do
     @name
@@ -23,6 +21,10 @@ class Dealership
 
   define_method(:cars) do
     @cars
+  end
+
+  define_singleton_method(:all) do
+    @@dealerships
   end
 
   define_method(:save) do
--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -2,59 +2,69 @@ require('rspec')
 require('dealership')
 
 describe(Dealership) do
+  before() do
+    Dealership.clear()
+  end
 
-  describe('.all') do
-    it('the dealership array is empty at first') do
-      expect(Dealership.all()).to(eq([]))
+  describe('#add_vehicle') do
+    it("adds a new vehicle to a dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      test_dealership.add_vehicle(test_vehicle)
+      expect(test_dealership.cars()).to(eq([test_vehicle]))
     end
   end
 
   describe('#name') do
-    it('returns the name of the dealership') do
-      test_dealership = Dealership.new("Flying Dragon Used Cars")
-      expect(test_dealership.name()).to(eq("Flying Dragon Used Cars"))
+    it("returns the name of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
+      expect(test_dealership.name()).to(eq("Bob's Used Cars"))
     end
   end
 
   describe('#id') do
-    it('returns the id of the dealership') do
-      test_dealership = Dealership.new("Flying Dragon Used Cars")
+    it("returns the id of the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
       expect(test_dealership.id()).to(eq(1))
     end
   end
 
   describe('#cars') do
-    it('adds an empty array of cars for the dealership') do
-      test_dealership = Dealership.new("Flying Dragon Used Cars")
+    it("initially returns an empty array of cars for the dealership") do
+      test_dealership = Dealership.new("Bob's Used Cars")
       expect(test_dealership.cars()).to(eq([]))
     end
   end
 
-  describe('#save') do
-    it('adds a dealership to the array of saved dealerships') do
-      test_dealership = Dealership.new("Flying Dragon Used Cars")
+  describe("#save") do
+    it("adds a dealership to the array of saved dealerships") do
+      test_dealership = Dealership.new("Bob's Used Cars")
       test_dealership.save()
       expect(Dealership.all()).to(eq([test_dealership]))
     end
   end
 
-  describe('.clear') do
-    it('empties out all of the saved dealerships') do
-      Dealership.new("Flying Dragon Used Cars").save()
+  describe(".all") do
+    it("is empty at first") do
+      expect(Dealership.all()).to(eq([]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved dealerships") do
+      Dealership.new("Bob's Used Cars").save()
       Dealership.clear()
       expect(Dealership.all()).to(eq([]))
     end
   end
 
-  describe('.find') do
-    it('returns a dealership by its id') do
-      test_dealership = Dealership.new('Flying Dragon Used Cars')
+  describe(".find") do
+    it("returns a dealership by its id number") do
+      test_dealership = Dealership.new("Bob's Used Cars")
       test_dealership.save()
-      second_dealership = Dealership.new('Cat Meowing Used Cars')
-      second_dealership.save()
+      test_dealership2 = Dealership.new("Jane's Cars")
+      test_dealership2.save()
       expect(Dealership.find(test_dealership.id())).to(eq(test_dealership))
     end
   end
-
-
 end
